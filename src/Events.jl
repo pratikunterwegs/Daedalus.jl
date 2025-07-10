54e97565993b7037a421a865468b9cc572c4844d
Pratik Gupte <pratikgupte16@gmail.com>
2025-07-10 16:13:12 +0100

    Functions to change and reset parameters

--- src/Events.jl
+++ src/Events.jl
@@ -2,9 +2,11 @@
 module Events
 
 using ..Constants
+using ..DaedalusStructs
 using DifferentialEquations
 
-export make_state_condition, make_time_condition, start_vax!, reduce_beta!
+export make_state_condition, make_time_condition, start_vax!, reduce_beta!,
+    make_time_events, get_times, make_param_changer, make_param_reset
 
 """
     make_cond(threshold)::Function
@@ -51,7 +53,7 @@ end
 An event function that reduces beta by a fixed value.
 """
 function reduce_beta!(integrator)
-    integrator.p.beta *= 0.8
+    integrator.p.beta *= 0.2
 end
 
 """
@@ -61,6 +63,49 @@ An event function that restores beta by a fixed value.
 """
 function restore_beta!(integrator)
     integrator.p.beta *= 1.6
+end
+
+"""
+    make_param_changer(param_name, func, coef)
+
+A function factory to generate effect functions.
+"""
+function make_param_changer(param_name, func, coef)
+    function effect!(integrator)
+        new_value = func(getproperty(integrator.p, Symbol(param_name)), coef)
+        setproperty!(integrator.p, Symbol(param_name * "_now"), new_value)
+    end
+end
+
+function make_param_reset(param_name)
+    function effect!(integrator)
+        original_value = getproperty(integrator.p, Symbol(param_name))
+        setproperty!(integrator.p, Symbol(param_name * "_now"), original_value)
+    end
+end
+
+"""
+    make_events(x::Npi)
+
+Make a CallbackSet from an Npi struct.
+"""
+make_time_events(x::Npi, effect_on::Function, effect_off::Function) = begin
+    npi_times_on = x.resparams.time_on
+    npi_times_off = x.resparams.time_off
+    coef = x.params.coef
+
+    cb_npi_on = DiscreteCallback(make_time_condition(npi_times_on), effect_on)
+    cb_npi_off = DiscreteCallback(make_time_condition(npi_times_off), effect_off)
+
+    return CallbackSet(cb_npi_on, cb_npi_off)
+end
+
+get_times(x::ResponseData) = begin
+    return [x.time_on; x.time_off]
+end
+
+get_times(x::Npi) = begin
+    return get_times(x.resparams)
 end
 
 end
