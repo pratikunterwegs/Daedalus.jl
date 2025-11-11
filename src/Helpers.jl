86250de0ba955a49873795329650ffe91ef0a988
Pratik Gupte <pratikgupte16@gmail.com>
2025-11-11 16:58:22 +0000

    Get beta from any size CM

--- src/Helpers.jl
+++ src/Helpers.jl
@@ -8,6 +8,7 @@ using ..Constants
 using LinearAlgebra
 
 function get_beta(cm, r0, sigma, p_sigma, epsilon, gamma_Ia, gamma_Is)
+    n_groups = size(cm)[1]
     sigma_1 = sigma * (1.0 - p_sigma)
     sigma_2 = sigma * p_sigma
 
@@ -15,25 +16,25 @@ function get_beta(cm, r0, sigma, p_sigma, epsilon, gamma_Ia, gamma_Is)
     foi_s = cm
 
     f_mat = hcat(
-        zeros(N_AGE_GROUPS, N_AGE_GROUPS),
+        zeros(n_groups, n_groups),
         foi_a, foi_s
     )
 
     vvec = [
-        repeat([sigma], N_AGE_GROUPS);
-        repeat([gamma_Ia], N_AGE_GROUPS);
-        repeat([gamma_Is], N_AGE_GROUPS)
+        repeat([sigma], n_groups);
+        repeat([gamma_Ia], n_groups);
+        repeat([gamma_Is], n_groups)
     ]
 
     v_mat = Matrix(Diagonal(vvec))
-    v_mat[(1:N_AGE_GROUPS).+N_AGE_GROUPS, 1:N_AGE_GROUPS] =
-        Matrix(Diagonal(repeat([-sigma_1], N_AGE_GROUPS)))
-    v_mat[(1:N_AGE_GROUPS).+N_AGE_GROUPS*2, 1:N_AGE_GROUPS] =
-        Matrix(Diagonal(repeat([-sigma_2], N_AGE_GROUPS)))
+    v_mat[(1:n_groups).+n_groups, 1:n_groups] =
+        Matrix(Diagonal(repeat([-sigma_1], n_groups)))
+    v_mat[(1:n_groups).+n_groups*2, 1:n_groups] =
+        Matrix(Diagonal(repeat([-sigma_2], n_groups)))
 
     v_inv = inv(v_mat)
 
-    ngm = f_mat * v_inv[:, 1:N_AGE_GROUPS]
+    ngm = f_mat * v_inv[:, 1:n_groups]
 
     r0a = maximum(real(eigen(ngm).values))
 
